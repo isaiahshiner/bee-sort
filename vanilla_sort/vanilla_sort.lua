0882de4aee9efd3a2cd8b01b6e109503c8b2fce0
Isaiah Shiner <shiner.isaiah@gmail.com>
2025-06-02 13:45:33 -0700

    Full vanilla demo works

--- vanilla_sort/vanilla_sort.lua
+++ vanilla_sort/vanilla_sort.lua
@@ -36,10 +36,12 @@ end
 local function getStacksAgnostic()
     if Top then
         local stacks = {}
+        local count = 0
         for slot, item in pairs(Top.getAllStacks()) do
             stacks[slot] = item.all()
+            count = count + 1
         end
-        return stacks
+        return stacks, count
     else
         return StaticExampleStacks
     end
@@ -58,52 +60,106 @@ ValueChart =
     ["emerald"] = 9,
 }
 
-local function getBreedSlot(stacks)
-    local bestSlot = -1
-    local bestValue = -1
+local function compareItems(a, b)
+    return a["value"] < b["value"]
+end
+
+local function getSortedItems(stacks)
+    local items = {}
+    local count = 0
     for slot, item in pairs(stacks) do
         local value = ValueChart[item.name]
-        if value and value > bestValue then
-            bestSlot = slot
-            bestValue = value
-        end
+        local slotMeta = { ["slot"] = slot, ["value"] = value }
+        table.insert(items, slotMeta)
+        count = count + 1
     end
-    return bestSlot
+    table.sort(items, compareItems)
+    return items, count
+end
+
+local function getBreedSlot(sortedItems, count)
+    return sortedItems[count]["slot"]
 end
 
 local function moveBreed(slot)
     if not Top then
-        print(slot)
+        print("Breed:", slot)
         return
     end
-
-    --There's no way to know if there's already an
-    --item in the dropper, so we'll just empty it
-    Top.pullItem(Direction, 1)
 
     -- Push an item from the main chest
     -- to the breeding dropper (direction)
     -- from chosen slot, and only move 1 item.
-    -- Always uses first available slot
+    -- Always uses first available slot in target
     Top.pushItem(Direction, slot, 1)
 end
 
-local function doTasks()
-    local stacks = getStacksAgnostic()
+local function getTrashSlots(sortedItems, count)
+    local trash = {}
+    local trashLimit = count - 12
+    if trashLimit <= 0 then return trash end
 
-    local slot = getBreedSlot(stacks)
+    for i = 1, trashLimit do
+        table.insert(trash, sortedItems[i]["slot"])
+    end
+    return trash
+end
+
+local function moveTrash(slots)
+    if not Top then
+        local out = "trash slots: "
+        for i, slot in ipairs(slots) do
+            out = out .. slot .. ", "
+        end
+        print(out)
+        return
+    end
+    for i, slot in ipairs(slots) do
+        Top.pushItem("up", slot)
+    end
+end
+
+local function doBreedCycle()
+    local stacks = getStacksAgnostic()
+    local sortedItems, count = getSortedItems(stacks)
+
+    local slot = getBreedSlot(sortedItems, count)
     moveBreed(slot)
+end
+
+LastCount = 0
+
+local function doTrashCycle()
+    local stacks, count = getStacksAgnostic()
+    if LastCount == count then
+        return
+    else
+        LastCount = count
+    end
+    local sortedItems, count = getSortedItems(stacks)
+    local slots = getTrashSlots(sortedItems, count)
+    moveTrash(slots)
 end
 
 startup("vanilla_sort/vanilla_res.lua")
 
 if Top then
-    doTasks()
-    while true do
-        os.pullEvent("redstone")
-        sleep(0.5) --ignore double pulse
-        doTasks()
+    local function breedLoop()
+        doBreedCycle()
+        while true do
+            os.pullEvent("redstone")
+            sleep(0.5) --ignore double pulse
+            doBreedCycle()
+        end
     end
+    local function trashLoop()
+        while true do
+            doTrashCycle()
+            sleep(1) -- don't kill PC
+        end
+    end
+    parallel.waitForAll(breedLoop, trashLoop)
 else
-    doTasks()
+    doBreedCycle()
+    doTrashCycle()
 end
